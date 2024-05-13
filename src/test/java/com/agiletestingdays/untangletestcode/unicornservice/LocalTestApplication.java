700077a8864eee301496e861fde7fcee38a9c6bd
Stefan Scheidt <stefan.scheidt@rewe-group.com>
2024-05-13 16:21:00 +0200

    Let PostgeSQLContainer also wait for listening port
    In some situations, TestContainers think PostgreSQLContainer is ready, but the port forwarding is not yet available.
    The adjusted wait strategy takes this into account.
    
    See also https://github.com/rancher-sandbox/rancher-desktop/issues/2609#issuecomment-1788871956

--- src/test/java/com/agiletestingdays/untangletestcode/unicornservice/LocalTestApplication.java
+++ src/test/java/com/agiletestingdays/untangletestcode/unicornservice/LocalTestApplication.java
@@ -1,10 +1,13 @@
 package com.agiletestingdays.untangletestcode.unicornservice;
 
+import java.time.Duration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
 import org.springframework.context.annotation.Bean;
 import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.containers.wait.strategy.Wait;
+import org.testcontainers.containers.wait.strategy.WaitAllStrategy;
 import org.testcontainers.junit.jupiter.Testcontainers;
 
 @SpringBootApplication
@@ -14,7 +17,15 @@ class LocalTestApplication {
   @Bean
   @ServiceConnection
   public PostgreSQLContainer<?> postgreSQLContainer() {
-    return new PostgreSQLContainer<>("postgres:16");
+    return new PostgreSQLContainer<>("postgres:16")
+        // see
+        // https://github.com/rancher-sandbox/rancher-desktop/issues/2609#issuecomment-1788871956
+        .waitingFor(
+            new WaitAllStrategy()
+                .withStrategy(Wait.forListeningPort())
+                .withStrategy(
+                    Wait.forLogMessage(".*database system is ready to accept connections.*\\s", 2)
+                        .withStartupTimeout(Duration.ofSeconds(60))));
   }
 
   public static void main(String[] args) {
