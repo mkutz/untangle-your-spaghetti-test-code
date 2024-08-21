7553f135a4b4743d81b90b7b01f2218dbf67f418
Michael Kutz <michael.kutz@rewe-group.com>
2024-08-21 17:58:12 +0200

    Remove unnecessary method

--- src/main/java/com/agiletestingdays/untangletestcode/unicornservice/adapter/driven/db/UnicornRepository.java
+++ src/main/java/com/agiletestingdays/untangletestcode/unicornservice/adapter/driven/db/UnicornRepository.java
@@ -1,12 +1,8 @@
 package com.agiletestingdays.untangletestcode.unicornservice.adapter.driven.db;
 
-import java.util.List;
 import java.util.UUID;
 import org.springframework.data.repository.ListCrudRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface UnicornRepository extends ListCrudRepository<UnicornEntity, UUID> {
-
-  List<UnicornEntity> findAll();
-}
+public interface UnicornRepository extends ListCrudRepository<UnicornEntity, UUID> {}
